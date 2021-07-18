94620a0ab04d6588c20feb0b9d87224c336a515d
atulkakrana <atulkakrana@gmail.com>
2021-07-18 22:47:24 +0000

    updated R script

--- scRNA-seq/README.md
+++ scRNA-seq/README.md
@@ -4,8 +4,13 @@
 1. Download sample speciific FASTQ files to local machine or AWS instances
 2. Use `count.sh` to generate gene-level counts and Loupe file for each sample
 3. Use `aggregate.sh` to combine counts from all sample-speciific counts (and a new Loupe file)
-4. 
+4. Explore expression and clusters using:
+    a. Loupe browser (load `cloupe` file from aggregation step), or
+    b. Use `scRNA.R` to explore the data
 
-
-### Identify Cell Types
+### Identify Cell-Types
 Loupe file from the "aggregation" step can be used to check for key markers for each cluster and assign a cell-type based on prior knowledge. Third-party computational tools developed for automated identification of cell types.
+
+Links:
+1. https://support.10xgenomics.com/single-cell-gene-expression/software/pipelines/latest/tutorials/gex-analysis-nature-publication#header
+2. https://satijalab.org/seurat/articles/pbmc3k_tutorial.html 
--- scRNA-seq/scRNA.R
+++ scRNA-seq/scRNA.R
@@ -1,133 +1,172 @@
 ## hands-on based on https://satijalab.org/seurat/v3.1/pbmc3k_tutorial.html
 ## publication: Comprehensive integration of single cell data (2018)
 
+## IMPORTS
 library(dplyr)
 library(Seurat)
+library(ggplot2)
 
-# Load the PBMC dataset
-pbmc.data = Read10X(data.dir = "~/GoogleDrive/0.learn/3.omics/1.scrna/filtered_gene_bc_matrices/hg19")
-pbmc      = CreateSeuratObject(counts = pbmc.data, project = "pbmc3k", min.cells = 3, min.features = 200)
-pbmc
+## USER SETTINGS
+EXP_NAME  = "E16_P0" ## Give some name to your expereimnt, used to create folders
+DATA_FOLD = "/home/anand/0.work/1.scseq/run_aggr_E16_5_P0/outs/count/filtered_feature_bc_matrix"
+
+
+## ANALYSIS
+
+# # Load the PBMC Example Dataset
+# pbmc.data = Read10X(data.dir = "~/GoogleDrive/0.learn/3.omics/1.scrna/filtered_gene_bc_matrices/hg19")
+# pbmc      = CreateSeuratObject(counts = pbmc.data, project = "pbmc3k", min.cells = 3, min.features = 200)
+# pbmc
+
+expr.data = Read10X(data.dir = DATA_FOLD)
+expr = CreateSeuratObject(counts = expr.data, project = EXP_NAME, min.cells = 3, min.features = 200)
+# expr[,1:10]
 
 ## Lets examine a few genes in the first thirty cells
-pbmc.data[c("CD3D", "TCL1A", "MS4A1"), 1:30]
+expr.data[c("Mid2", "Rbm41"), 1:30] ## change to any genes that exist in data
 
 ## The [[ operator can add columns to object metadata. This is a great place to stash QC stats
-pbmc[["percent.mt"]] <- PercentageFeatureSet(pbmc, pattern = "^MT-")
+expr[["percent.mt"]] <- PercentageFeatureSet(expr, pattern = "^mt-") ## Try "^MT-" if all values as same error
 
 ## Show QC metrics for the first 5 cells
-head(pbmc@meta.data, 5)
+head(expr@meta.data, 5)
 
 ## Visualize QC metrics as a violin plot
-VlnPlot(pbmc, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+VlnPlot(expr, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
 
 ## FeatureScatter is typically used to visualize feature-feature relationships, but can be used
 ## for anything calculated by the object, i.e. columns in object metadata, PC scores etc.
-plot1 = FeatureScatter(pbmc, feature1 = "nCount_RNA", feature2 = "percent.mt")
-plot2 = FeatureScatter(pbmc, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+plot1 = FeatureScatter(expr, feature1 = "nCount_RNA", feature2 = "percent.mt")
+plot2 = FeatureScatter(expr, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+ggsave("feats.png" , plot = last_plot(), dpi = 300, bg = "white") ## set bg = NULL for transparent background
 CombinePlots(plots = list(plot1, plot2))
+# ggsave("feats.png" , plot = last_plot(), dpi = 300, bg = "white") ## set bg = NULL for transparent background
 
 ## filter cells that have unique feature counts over 2,500 or less than 200
 ## filter cells that have >5% mitochondrial counts
-pbmc = subset(pbmc, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5)
+expr = subset(expr, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5)
 
 #### NORMALIZE DATA ############################################
 ## global-scaling normalization method “LogNormalize” that normalizes the feature 
 ## expression measurements for each cell by the total expression, multiplies this 
 ## by a scale factor (10,000 by default), and log-transforms the result
-pbmc = NormalizeData(pbmc, normalization.method = "LogNormalize", scale.factor = 10000)
+expr  = NormalizeData(expr, normalization.method = "LogNormalize", scale.factor = 10000)
 
 ## calculate a subset of features that exhibit high cell-to-cell variation in the dataset
-pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 2500)
+expr = FindVariableFeatures(expr, selection.method = "vst", nfeatures = 2500)
 
 ## Identify the 10 most highly variable genes
-top10 <- head(VariableFeatures(pbmc), 10)
+top20 = head(VariableFeatures(expr), 20)
+top20
 
 ## plot variable features with and without labels
-plot1 <- VariableFeaturePlot(pbmc)
-plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
-CombinePlots(plots = list(plot1, plot2))
+plot1 <- VariableFeaturePlot(expr)
+ggsave("vfp.png" , plot = last_plot(), dpi = 300, bg = "white") ## set bg = NULL for transparent background
+
+plot2 <- LabelPoints(plot = plot1, points = top20, repel = TRUE)
+ggsave("vfp_labelled.png" , plot = last_plot(), dpi = 300, bg = "white") ## set bg = NULL for transparent background
+
+# CombinePlots(plots = list(plot1, plot2))
+# ggsave("vfp_final.png" , plot = last_plot(), dpi = 300, 
+#             width=14, height=8, bg = "white") ## set bg = NULL for transparent background
 
 ## SCALING THE DATA ############################################
 ## step gives equal weight in downstream analyses, so that highly-expressed genes do not dominate;
 ## to ‘regress out’ heterogeneity associated with (for example) cell cycle stage, or mitochondrial
 ## contamination see https://satijalab.org/seurat/v3.0/sctransform_vignette.html
-all.genes = rownames(pbmc)
-pbmc      = ScaleData(pbmc, features = all.genes)
+all.genes = rownames(expr)
+expr      = ScaleData(expr, features = all.genes)
 
 
 #### DIMENSIONALITY REDUCTION ###################################
 ## perform PCA on the scaled data, and
 ## examine and visualize PCA results
-pbmc = RunPCA(pbmc, features = VariableFeatures(object = pbmc))
-print(pbmc[["pca"]], dims = 1:5, nfeatures = 5)
-VizDimLoadings(pbmc, dims = 1:2, reduction = "pca")
-DimPlot(pbmc, reduction = "pca")
+expr = RunPCA(expr, features = VariableFeatures(object = expr))
+print(expr[["pca"]], dims = 1:5, nfeatures = 5)
+VizDimLoadings(expr, dims = 1:2, reduction = "pca")
+
+DimPlot(expr, reduction = "pca")
+ggsave("pca.png" , plot = last_plot(), dpi = 300, bg = "white")
 
 
 ## Determine the ‘dimensionality’ of the dataset
 # NOTE: This process can take a long time for big datasets, comment out for expediency. More
 # approximate techniques such as those implemented in ElbowPlot() can be used to reduce
 # computation time
-pbmc = JackStraw(pbmc, num.replicate = 100)
-pbmc = ScoreJackStraw(pbmc, dims = 1:20)
+expr = JackStraw(expr, num.replicate = 100)
+expr = ScoreJackStraw(expr, dims = 1:20)
 
 ## provides a visualization tool for comparing the distribution of p-values 
 ## for each PC with a uniform distribution (dashed line)
-JackStrawPlot(pbmc, dims = 1:15)
+JackStrawPlot(expr, dims = 1:15)
+ggsave("jackStrawPlot.png" , plot = last_plot(), dpi = 300, bg = "white")
 
 ## alternative heuristic method generates an ‘Elbow plot’: a ranking of principle components based 
 ## on the percentage of variance explained by each one
-ElbowPlot(pbmc)
+ElbowPlot(expr)
+ggsave("ElbowPlot.png" , plot = last_plot(), dpi = 300, bg = "white")
 
 
 #### CLUSTER CELLS #############################################
-
 ## embed cells in a graph structure - for example a K-nearest neighbor (KNN) graph, 
 ## with edges drawn between cells with similar feature expression patterns, and then 
 ## attempt to partition this graph into highly interconnected ‘quasi-cliques’ or ‘communities’
-pbmc <- FindNeighbors(pbmc, dims = 1:10)
-pbmc <- FindClusters(pbmc, resolution = 0.5)
+expr = FindNeighbors(expr, dims = 1:10)
+expr = FindClusters(expr, resolution = 0.5)
 
 # Look at cluster IDs of the first 5 cells
-head(Idents(pbmc), 5)
+head(Idents(expr), 5)
 
 ## non-linear dimensional reduction techniques, such as tSNE and UMAP
 ## Examine and nd visualize tSNE results
-pbmc = RunTSNE(object = pbmc)
-DimPlot(pbmc, reduction = "tsne")
+expr = RunTSNE(object = expr)
+DimPlot(expr, reduction = "tsne")
+ggsave("tsne.png" , plot = last_plot(), dpi = 600, bg = "white")
 
 ## Examine and nd visualize umap results
-pbmc = RunUMAP(object = pbmc, dims = 1:10)
-DimPlot(pbmc, reduction = "umap")
+expr = RunUMAP(object = expr, dims = 1:10)
+DimPlot(expr, reduction = "umap")
+ggsave("umap.png" , plot = last_plot(), dpi = 600, bg = "white")
 
 
 #### FIND DIFFRENTIALLY EXPRESSED FEATURES ###################
-## dentifes positive and negative markers of a single cluster (specified in ident.1),
+## identifes positive and negative markers of a single cluster (specified in ident.1),
 ## compared to all other cells. ## `min.pct`` argument requires a feature to be detected 
 ## at a minimum percentage in either of the two groups of cells. `thresh.test`` argument 
 ## requires a feature to be differentially expressed (on average) by some amount between the two groups.
-## Find all markers of cluster 1
-cluster1.markers <- FindMarkers(pbmc, ident.1 = 1, min.pct = 0.25)
-head(cluster1.markers, n = 5)
 
-# find all markers distinguishing cluster 5 from clusters 0 and 3
-cluster5.markers <- FindMarkers(pbmc, ident.1 = 5, ident.2 = c(0, 3), min.pct = 0.25)
-head(cluster5.markers, n = 5)
+## Find all markers of provided cluster
+clust_x = 6
+clusterX.markers <- FindMarkers(expr, ident.1 = clust_x, min.pct = 0.25)
+head(clusterX.markers, n = 10)
 
-# find markers for every cluster compared to all remaining cells, report only the positive ones
-pbmc.markers <- FindAllMarkers(pbmc, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
-pbmc.markers %>% group_by(cluster) %>% top_n(n = 2, wt = avg_logFC)
+## Find all markers distinguishing cluster in ident.1 from clusters in ident.2
+clust_x = 6
+clust_y = c(0, 3)
+clusterX.markers <- FindMarkers(expr, ident.1 = clust_x, ident.2 = clust_y, min.pct = 0.25)
+head(clusterX.markers, n = 10)
 
-# different tests for differential expression
-cluster1.markers <- FindMarkers(pbmc, ident.1 = 0, logfc.threshold = 0.25, test.use = "roc", only.pos = TRUE)
+## Find markers for every cluster compared to all remaining cells, report only the positive ones
+avg_logFC_cutoff = 1
+expr.markers <- FindAllMarkers(expr, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
+expr.markers %>% group_by(cluster) %>% top_n(n = 2, wt = avg_logFC_cutoff)
+write.csv(expr.markers, file = "top_markers.tsv",row.names=TRUE, sep = "\t")
 
+## Different tests for differential expression
+clust_x = 6
+clusterX.markers <- FindMarkers(expr, ident.1 = clust_x, logfc.threshold = 0.25, test.use = "roc", only.pos = TRUE)
+
+
+
+## VISUALIZATION
 ## visualize expression for different genes
-VlnPlot(pbmc, features = c("MS4A1", "CD79A"))
-# you can plot raw counts as well
-VlnPlot(pbmc, features = c("NKG7", "PF4"), slot = "counts", log = TRUE)
-## visualizes feature expression on a tSNE or PCA plot
-FeaturePlot(pbmc, features = c("MS4A1", "GNLY", "CD3E", "CD14", "FCER1A", "FCGR3A", "LYZ", "PPBP", 
-                               "CD8A"))
-library(ggplot2)
+VlnPlot(expr, features = c("Mafg", "Hmox1"))
+ggsave("VlnPlot.png" , plot = last_plot(),   dpi = 300, bg = "white", width=12, height=8)
 
+## Plot raw counts as well
+VlnPlot(expr, features = c("Mafg", "Hmox1"), slot = "counts", log = TRUE)
+ggsave("VlnPlotRawCounts.png" , plot = last_plot(), dpi = 300, bg = "white", width=12, height=8)
+
+## Visualizes feature expression on a tSNE or PCA plot
+FeaturePlot(expr, features = c("Mafg", "Hmox1"))
+ggsave("FeatsExpression.png" , plot = last_plot(), dpi = 300, bg = "white", width=12, height=8)
